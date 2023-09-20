5a7f4943ca7d6416a17deb6b8e462fb57c6dc601
SaidARSLAN <lost.myreligion@hotmail.com>
2023-09-20 11:49:36 +0300

    everything has been fixed

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -4,9 +4,9 @@ import ProjectCard from '../components/ProjectCard'
 
 const Projects = () => {
   return (
-    <section className='flex flex-col text-center'>
-      <h3 className='text-lg font-poppins text-pOrange'>PORTFOLIO</h3>
-      <h3 className='text-2xl my-4 font-poppins '>I am proud of the projects I have worked on.</h3>
+    <section className='flex flex-col'>
+      <h3 className='text-lg font-poppins text-center text-pOrange lg:text-left'>PORTFOLIO</h3>
+      <h3 className='text-2xl my-4 font-poppins text-center lg:text-left text-pBlack'>I am proud of the projects I have worked on.</h3>
       <div>
         {projects.map((project) => {
             return <ProjectCard project={project} />
